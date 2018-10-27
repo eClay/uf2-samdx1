ed591301073aa49e7fcd38bbc9096185dbee9786
Greg Mrozek <greg.mrozek@eclay.com>
2018-10-26 20:56:12 -0500

    Add eClay minion_samd21 board. Minor changes to linker script to use named values for memory locations.

---
+++ boards/minion_samd21/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "eClay Inc."
+#define PRODUCT_NAME "Minion D21"
+#define VOLUME_LABEL "KEVIN"
+#define INDEX_URL "http://eclay.com/"
+#define BOARD_ID "SAMD21G18A-Minion-v0"
+
+#define USB_VID 0xEC1A
+#define USB_PID 0x8B0B
+
+#define LED_PIN PIN_PA31
+
+#endif
